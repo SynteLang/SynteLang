4b74731e1e07af35e2fc7c82495c458d8ea0c4af
SynteLang <synte@proton.me>
2023-05-28 12:46:30 +0100

    adding "-u" flag to functions.go to process operators usage

--- functions.go
+++ functions.go
@@ -4,9 +4,12 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 )
 
 // terminal colours
@@ -36,6 +39,29 @@ func main() {
 	err2 := json.Unmarshal(Json, &functions)
 	if err != nil || err2 != nil {
 		fmt.Printf("error loading %s: %v %v", file, err, err2)
+	}
+	if len(os.Args) > 1 && os.Args[1] == "-u" {
+		u := loadUsage()
+		for name, n := range u {
+			// for each function in usage list
+			if _, in := functions[name]; !in {
+				continue
+			}
+			// for each operator in function count usage
+			ops := map[string]int{}
+			for _, f := range functions[name] {
+				ops[f.Op]++
+			}
+			// multiply each op count by function count
+			// and add to op count
+			for op := range ops {
+				u[op] += ops[op] * n
+			}
+			// delete function from usage list
+			delete(u, name)
+		}
+		fmt.Println(sortUsage(u))
+		return
 	}
 	fmt.Printf("\n%s%sfunctions%s\n", yellow, italic, reset)
 	fmt.Println()
@@ -76,4 +102,50 @@ func main() {
 	for k := range functions {
 		fmt.Printf("\t%s%s%s ", italic, k, reset)
 	}
+}
+
+func loadUsage() map[string]int {
+	u := map[string]int{}
+	f, err := os.Open("usage.txt")
+	if err != nil {
+		return u
+	}
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanWords)
+	for s.Scan() {
+		op := s.Text()
+		if op == "unused:" {
+			break
+		}
+		s.Scan()
+		n, err := strconv.Atoi(s.Text())
+		if err != nil {
+			continue
+		}
+		u[op] = n
+	}
+	return u
+}
+type pair struct {
+	Key   string
+	Value int
+}
+type pairs []pair
+
+func (p pairs) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p pairs) Len() int           { return len(p) }
+func (p pairs) Less(i, j int) bool { return p[i].Value > p[j].Value }
+func sortUsage(u map[string]int) string {
+	p := make(pairs, len(u))
+	i := 0
+	for k, v := range u {
+		p[i] = pair{k, v}
+		i++
+	}
+	sort.Sort(p)
+	data := ""
+	for i, s := range p {
+		data += fmt.Sprintf("%d %d %s\n", i, s.Value, s.Key)
+	}
+	return data
 }

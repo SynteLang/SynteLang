f307955d61244ce5a74c985836626b9bea528e03
SynteLang <synte@proton.me>
2023-11-01 21:22:07 +0000

    correcting mute on reload

--- bsd-linux.go
+++ bsd-linux.go
@@ -668,9 +668,7 @@ func loadReloadAppend(t *systemState) int {
 		reload = n
 		if len(mutes) > reload {
 			mutes[reload] = 0
-		}
-		if reload > len(transfer.Listing)-1 {
-			time.Sleep(25*time.Millisecond)
+			time.Sleep(50*time.Millisecond)
 		}
 		t.operand = ".temp/" + t.operand
 	case "apd":

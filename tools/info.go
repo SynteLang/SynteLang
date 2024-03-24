b5db0ac9dc89d3c6c22bdcead7ca79f5ac47311a
SynteLang <synte@proton.me>
2024-03-24 18:33:06 +0000

    formatting

--- tools/info.go
+++ tools/info.go
@@ -194,7 +194,7 @@ func main() {
 %s
 %s
 %s
-      %sMouse-X:%s %5.5g       %sMouse-Y:%s %5.5g
+      %sMouse-X:%s %5.4g       %sMouse-Y:%s %5.4g
 ╰───────────────────────────────────────────────────╯`,
 				sync, paused, timer,
 				yellow, reset, L, display.Mode, soundcard,

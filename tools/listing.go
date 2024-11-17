dad73f8a4bd0f962d02f0a0fac5938ef0722c25c
SynteLang <synte@proton.me>
2024-11-17 21:05:59 +0000

    rationalising for better display. Listings will only reprint when updated. "/*" comments will display names in margin

--- tools/listing.go
+++ tools/listing.go
@@ -1,5 +1,5 @@
 /*	listing.go displays active listings while Syntə is running
-	Data transferred via './displaylisting.json'
+	Data transferred via './displaylisting.json' and 'infodisplay.json'
 	File emptied on exit. Check '/recordings' folder to see played listings by timestamp
 	Press enter to exit
 */
@@ -9,6 +9,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"slices"
 	"os"
 	"time"
 )
@@ -26,106 +27,133 @@ const (
 	cyan    = "\x1b[36m"
 )
 
-func main() {
+type muteVerb struct {
+	mute    []bool
+	verbose bool
+}
 
+func main() {
+	var (
+		file = "displaylisting.json"
+		exit bool
+		stop = make(chan struct{})
+	)
+
+	go func() {
+		var (
+			t time.Time
+			mv muteVerb
+		)
+		for !exit {
+			st, _ := os.Stat(file)
+			lt := st.ModTime()
+			mvn, ch := mutesOrVerboseChanged(mv)
+			if !lt.Equal(t) || ch {
+				fmt.Printf("\n\n\033[H\033[2J")
+				readAndDisplay(file, mvn)
+			}
+			t = lt
+			mv = mvn
+			time.Sleep(300 * time.Millisecond)
+		}
+		close(stop)
+	}()
+	fmt.Scanln()
+	exit = true
+	<-stop
+	fmt.Printf("display listing closed.\n")
+}
+
+func mutesOrVerboseChanged(mv muteVerb) (muteVerb, bool) {
+	file2 := "infodisplay.json"
+	d := make(map[string]json.RawMessage)
+	Json, err := os.ReadFile(file2)
+	err2 := json.Unmarshal(Json, &d)
+	if err != nil || err2 != nil {
+		//fmt.Printf("error loading %s: %v %v\n", file2, err, err2)
+		//time.Sleep(2 * time.Second)
+	}
+	var m []bool
+	err2 = json.Unmarshal(d["Mute"], &m)
+	if err2 != nil {
+		//fmt.Printf("error decoding %s: %v %v\n", file2, err, err2)
+		//time.Sleep(2 * time.Second)
+	}
+	var v bool
+	err2 = json.Unmarshal(d["Verbose"], &v)
+	if err2 != nil {
+		//fmt.Printf("error decoding %s: %v %v\n", file2, err, err2)
+		//time.Sleep(2 * time.Second)
+	}
+	if slices.Equal(mv.mute, m) && mv.verbose == v {
+		return mv, false
+	}
+	return muteVerb{m, v}, true
+}
+
+func readAndDisplay(file string, info muteVerb) {
 	var listing [][]struct {
 		Op  string
 		Opd string
 	}
 
-	file := "displaylisting.json"
-	file2 := "infodisplay.json"
-	d := make(map[string]json.RawMessage)
+	Json, err := os.ReadFile(file)
+	err2 := json.Unmarshal(Json, &listing)
+	if err != nil {
+		//fmt.Printf("error loading %s: %v %v\n", file, err, err2)
+		//time.Sleep(2 * time.Second)
+	}
+	if err2 != nil {
+		//fmt.Printf("error decoding %s: %v %v\n", file, err, err2)
+		//time.Sleep(2 * time.Second)
+	}
+	fmt.Printf("%sSyntə listings%s %spress enter to quit%s", cyan, reset, italic, reset)
 
-	var exit bool
-	stop := make(chan struct{})
-	var mute []bool
-	var verbose bool
-
-	go func() {
-		for {
-			Json, err := os.ReadFile(file)
-			err2 := json.Unmarshal(Json, &listing)
-			if err != nil {
-				//fmt.Printf("error loading %s: %v %v\n", file, err, err2)
-				//time.Sleep(2 * time.Second)
-			}
-			if err2 != nil {
-				//fmt.Printf("error decoding %s: %v %v\n", file, err, err2)
-				//time.Sleep(2 * time.Second)
-			}
-			Json, err = os.ReadFile(file2)
-			err2 = json.Unmarshal(Json, &d)
-			if err != nil || err2 != nil {
-				//fmt.Printf("error loading %s: %v %v\n", file2, err, err2)
-				//time.Sleep(2 * time.Second)
-			}
-			err2 = json.Unmarshal(d["Mute"], &mute)
-			if err2 != nil {
-				//fmt.Printf("error decoding %s: %v %v\n", file2, err, err2)
-				//time.Sleep(2 * time.Second)
-			}
-			err2 = json.Unmarshal(d["Verbose"], &verbose)
-			if err2 != nil {
-				//fmt.Printf("error decoding %s: %v %v\n", file2, err, err2)
-				//time.Sleep(2 * time.Second)
-			}
-			fmt.Printf("\033[H\033[2J")
-			fmt.Printf("%sSyntə listings%s %spress enter to quit%s", cyan, reset, italic, reset)
-			//fmt.Println()
-
-			for i, list := range listing {
-				if len(list) < 1 {
-					continue
-				}
-				if list[0].Op == "deleted" {
-					continue
-				}
-				fmt.Printf("\n%d:\t", i)
-				m, c, y := magenta, cyan, yellow
-				if len(mute) >= i+1 { // bounds check
-					if mute[i] {
-						m, c, y = italic, italic, italic
-					}
-				}
-				for i, v := range list {
-					if verbose {
-						fmt.Printf(" %s%d:%s ", italic, i, reset)
-					}
-					mm := m
-					switch v.Op {
-					case "noise", "sino", "saw", "sqr", "pulse":
-						mm = y
-					}
-					fmt.Printf("%s%s%s", mm, v.Op, reset)
-					if opd := v.Opd; opd != "" {
-						fmt.Printf(" %s%s%s", c, opd, reset)
-					}
-					if i == len(list)-1 || verbose {
-						continue
-					}
-					switch list[i+1].Op {
-					case "in", "pop", ")", "index", "ifft", "/b", "all":
-						//fmt.Printf(" %s|%s  ", y, reset)
-						fmt.Printf("\n\t")
-					default:
-						//fmt.Printf(" %s\u22A2%s  ", y, reset)
-						fmt.Printf("%s,%s  ", italic, reset)
-					}
-				}
-			}
-			time.Sleep(500 * time.Millisecond)
-			if exit {
-				close(stop)
-				break
+	for i, list := range listing {
+		if len(list) < 1 {
+			continue
+		}
+		if list[0].Op == "deleted" {
+			continue
+		}
+		fmt.Printf("\n%d ", i)
+		if list[0].Op == "/*" {
+			fmt.Printf("%s", list[0].Opd)
+		}
+		fmt.Printf("\t")
+		m, c, y := magenta, cyan, yellow
+		if i < len(info.mute) { // bounds check
+			if info.mute[i] {
+				m, c, y = italic, italic, italic
 			}
 		}
-	}()
-	if !exit {
-		fmt.Printf("press enter to quit")
-		fmt.Scanln()
-		exit = true
-		<-stop
+		for i, v := range list {
+			if i == 0 && list[0].Op == "/*" {
+				continue
+			}
+			if info.verbose {
+				fmt.Printf(" %s%d:%s ", italic, i, reset)
+			}
+			mm := m
+			switch v.Op {
+			case "noise", "sino", "saw", "sqr", "pulse":
+				mm = y
+			}
+			fmt.Printf("%s%s%s", mm, v.Op, reset)
+			if opd := v.Opd; opd != "" {
+				fmt.Printf(" %s%s%s", c, opd, reset)
+			}
+			if i == len(list)-1 || info.verbose {
+				continue
+			}
+			switch list[i+1].Op {
+			case "in", "pop", ")", "index", "ifft", "/b", "all":
+				//fmt.Printf(" %s|%s  ", y, reset)
+				fmt.Printf("\n\t")
+			default:
+				//fmt.Printf(" %s\u22A2%s  ", y, reset)
+				fmt.Printf("%s,%s  ", italic, reset)
+			}
+		}
 	}
-	fmt.Printf("display listing closed.\n")
 }
